002504920f53d3fdfc80c564a9ac9ffa5e5c2eef
Grygoriy Zholtkevych <g.zholtkevych@karazin.ua>
2022-02-14 00:12:16 +0200

    Update Concepts.v

--- Concepts.v
+++ Concepts.v
@@ -3,58 +3,49 @@ Require Import Lists.List.
 Import ListNotations.
 
 
-Class Category_Axioms
+Class Category_axiomatics
         (object : Type)
         (arrow : object → object → Type)
         (id : ∀ x : object, arrow x x)
         (compose : 
-          ∀ (x y z : object) (f : arrow x y) (g : arrow y z), arrow x z)
-            : Prop :=
-  { id_is_id : ∀ (x y : object) (f : arrow x y), 
-                 compose x x y (id x) f = f /\
                  compose x y y f (id y) = f
   ; associativity : True
+        compose x x y (id x) f = f ∧ compose x y y f (id y) = f
+  ; associativity :
+       ∀ (x y z u : object) (f : arrow x y) (g : arrow y z) (h : arrow z u),
+        (compose x z u (compose x y z f g) h) =
+          (compose x y u f (compose y z u g h))
+}.
+
+Structure Category : Type := defCategory
+{ object : Type
+; arrow : object → object → Type
+; id : ∀ x : object, arrow x x
+; compose : 
+     ∀ (x y z : object) (f : arrow x y) (g : arrow y z), arrow x z
+; cat_cert : Category_axiomatics object arrow id compose
 }.
 
 
-Module CAT.
-Structure Quiver : Type := dfCat
-  { object : Type
-  ; arrow : Type
-  ; src : arrow → object
-  ; trg : arrow → object
-  ; id : object -> arrow
-}.
+Lemma le_refl : ∀ n : nat, n ≤ n.
+Proof. constructor. Qed.
 
-Record Composition {q : Quiver} (f g : arrow q) := defComposition
-  { _ : trg q f = src q g
-  ; ra : arrow q
-  ; _ : src q ra = src q f /\ trg q ra = trg q g
-}.
+Lemma le_trans : ∀ n m k : nat, n ≤ m → m ≤ k → n ≤ k.
+Proof.
+  intros * H1 H2.
+  induction H2 as [| m' IH2].
+  - assumption.
+  - inversion H1; now constructor.
+Qed.
 
-Definition compose
-             {q : Quiver}
-             (f g : arrow q)
-             {c : @Composition q f g} : arrow q := ra f g c.
+Instance nat_category_axiomatics : Category_axiomatics
+  nat (fun (n m : nat) => n ≤ m) le_refl le_trans.
+Proof.
+  constructor.
+  - intros. split. 
 
 
-Class Category_Axioms 
-        {q : Quiver}
-        {c : ∀ f g : arrow q, Composition f g } : Prop :=
-{ id_src_cert : ∀ x : object q, src q (id q x) = x /\ trg q (id q x) = x
-; id_law_cert : ∀ f : arrow q,
-    @compose q f (id q (src q f)) (c f (id q (src q f))) = f /\
-    @compose q (id q (trg q f)) f (c (id q (trg q f)) f) = f
-; associativity_law : ∀ f g h : arrow q,
-    let gf := @compose q f g (c f g) in
-    let hg := @compose q g h (c g h) in
-    @compose q gf h (c gf h) = @compose q f hg (c f hg)
-}.
-End CAT.
-
-Notation Quiver := CAT.Quiver  (only parsing).
-
-Definition nat_quiver := {f : nat * nat | fst f ≤ snd f}.
+Definition nat_object := {f : nat * nat | fst f ≤ snd f}.
 Definition nat_src (f : nat_arrow) : nat
 := match f with exist _ sf _ => fst sf end.
 Definition nat_trg (f : nat_arrow) : nat
